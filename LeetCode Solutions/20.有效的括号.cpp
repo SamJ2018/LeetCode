784bd0b1491050bbd80f5a0e2420467b63152d8f
juksam <494403786@qq.com>
2021-02-07 00:15:01 +0800

    complete 1-10

---
+++ LeetCode Solutions/20.有效的括号.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=cpp
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (43.62%)
+ * Likes:    2109
+ * Dislikes: 0
+ * Total Accepted:    511.1K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 
+ * 有效字符串需满足：
+ * 
+ * 
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 注意空字符串可被认为是有效字符串。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "()"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "()[]{}"
+ * 输出: true
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: "(]"
+ * 输出: false
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: "([)]"
+ * 输出: false
+ * 
+ * 
+ * 示例 5:
+ * 
+ * 输入: "{[]}"
+ * 输出: true
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isValid(string s) {
+
+    }
+};
+// @lc code=end
+
---
+++ LeetCode Solutions/7.整数反转.cpp
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=cpp
+ *
+ * [7] 整数反转
+ *
+ * https://leetcode-cn.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (34.87%)
+ * Likes:    2449
+ * Dislikes: 0
+ * Total Accepted:    555.2K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '123'
+ *
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * 
+ * 
+ * 
+ * 注意：
+ * 
+ * 
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回
+ * 0。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：x = 123
+ * 输出：321
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：x = -123
+ * 输出：-321
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：x = 120
+ * 输出：21
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：x = 0
+ * 输出：0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * -2^31 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int reverse(int x) {
+
+    }
+};
+// @lc code=end
+
